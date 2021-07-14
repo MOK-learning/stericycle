6c525cdbf4904b75866b04e84e4b7f0d8502c229
Michael William O'Keefe <michael.william.o'keefe@cern.ch>
2021-07-14 13:52:20 +0100

    done product/services dashboard

--- dashboard.py
+++ dashboard.py
@@ -26,8 +26,24 @@ application = app.server
 # Open the csv and get list of runs
 df = pd.read_csv('data/CustomerChurn.csv')
 
+# Make sure Monthly and Total Charges are floating point
+df.MonthlyCharges = pd.to_numeric(df.MonthlyCharges, errors='coerce')
+df.TotalCharges   = pd.to_numeric(df.TotalCharges, errors='coerce')
+
+# Replace NaN with median
+df.tenure.fillna(df.tenure.median(), inplace=True)
+df.MonthlyCharges.fillna(df.MonthlyCharges.median(), inplace=True)
+df.TotalCharges.fillna(df.TotalCharges.median(), inplace=True)
+
 # Rename SeniorCitizen 0/1 as No/Yes
 df.SeniorCitizen = df.SeniorCitizen.map({0: 'No', 1: 'Yes'})
+
+# Define Streaming (TV OR Movies) column
+df['Streaming'] = 'No'
+
+m1 = (df['StreamingTV'] == 'Yes')
+m2 = (df['StreamingMovies'] == 'Yes')
+df['Streaming'] = df['Streaming'].mask(m1 | m2, 'Yes')
 
 
 # Define the layout of our dashboard
@@ -110,9 +126,61 @@ app.layout = html.Div([
     Customers who do not have partners are about 30% more likely to churn;
     senior citizens are about 50% less likely to churn; 
     customers without dependents are 55% more likely to churn.
+    
     Now that we have looked at demographic factors, 
     what about the products which the customers are using?
     Let's find out if this has any impact on the propensity of customers to churn.
+    There are many products on offer, we will focus here on the main ones: 
+    phone service, internet service and streaming (TV or Movies).
+    ''', style={'text-align': 'center'}),
+
+    
+    # Product churn graphs
+    html.Div(children=[
+        dcc.Graph(id='pie-churn-phone', 
+                  figure=px.histogram(df, x='Churn', color='PhoneService', title='Phone service churn', barnorm='percent'),
+                  style={'display': 'inline-block', 'width': '33%'}
+                 ), 
+        dcc.Graph(id='pie-churn-internet', 
+                  figure=px.histogram(df, x='Churn', color='InternetService', title='Internet service churn', barnorm='percent'),
+                  style={'display': 'inline-block', 'width': '33%'}
+                 ), 
+        dcc.Graph(id='pie-churn-streaming', 
+                  figure=px.histogram(df, x='Churn', color='Streaming', title='Streaming service churn', barnorm='percent'),
+                  style={'display': 'inline-block', 'width': '33%'}
+                 ), 
+    ], style={'text-align': 'center'}),
+    
+    dcc.Markdown('''
+    Whether or not a customer uses a phone service does not seem to effect
+    whether or not they will churn. 
+    Fibre optic internet users are most likely to churn out of internet service users, 
+    about 40% more likely to churn than other users. 
+    Customers who use streaming services are roughly 13% more likely to churn than those that do not.
+
+    Finally we will look at the financial aspect, based on a customers tenure, 
+    monthly charges and total charges.
+    ''', style={'text-align': 'center'}),
+    
+
+    # Financial churn graphs
+    html.Div(children=[
+        dcc.Graph(id='churn-tenure', 
+                  #figure=px.histogram(df, x='Churn', color='tenure', title='Customer tenure churn', barnorm='percent'),
+                  style={'display': 'inline-block', 'width': '33%'}
+                 ), 
+        dcc.Graph(id='churn-month', 
+                  #figure=px.histogram(df, x='Churn', color='MonthlyCharges', title='Monthly charges churn', barnorm='percent'),
+                  style={'display': 'inline-block', 'width': '33%'}
+                 ), 
+        dcc.Graph(id='churn-total', 
+                  #figure=px.histogram(df, x='Churn', color='TotalCharges', title='Total charges churn', barnorm='percent'),
+                  style={'display': 'inline-block', 'width': '33%'}
+                 ), 
+    ], style={'text-align': 'center'}),
+    
+    dcc.Markdown('''
+    Placeholder...
     ''', style={'text-align': 'center'}),
 
 
